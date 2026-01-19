61835f03d8c70212cbcc7d906b430b60286fface
infrabyanubhav <infrabyanubhav@gmail.com>
2026-01-19 14:41:42 +0530

    push

--- inventory/tests.py
+++ inventory/tests.py
@@ -13,8 +13,14 @@ class CompanyApiTestCase(TestCase):
     Test case for the Company API.
     """
     def setUp(self):
+        """
+        Set up the test client.
+        """
         self.client = APIClient()
     def test_get_companies(self):
+        """
+        Test the get companies API.
+        """
         response = self.client.get(reverse("get_low_stock_alerts", kwargs={"company_id": 1}))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["total_alerts"], len(response.data["alerts"]))
